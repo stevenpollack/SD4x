c94203569161a9252523d99e7ed7cb76dfed9f12
stevenpollack <stevenpollack@users.noreply.github.com>
2020-03-31 19:23:22 +1100

    updated .gitignore
    to ignore node_modules and .vscode settings

--- hw4/FontChooser.js
+++ hw4/FontChooser.js
@@ -1,3 +1,22 @@
+class ButtonClick extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {count: 0};
+	}
+
+	handleClick() {
+		this.setState({count: this.state.count +1});
+	}
+
+	render() {
+		var count = this.state.count;
+		<span>{count}</span>
+		<button onClick={this.handleClick.bind(this)}>Increment!</button>
+
+	}
+
+}
+
 class FontChooser extends React.Component {
 
     constructor(props) {
