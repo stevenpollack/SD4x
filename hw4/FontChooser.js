5bee916dccb061b11ea967bc3bb9745d1dab063e
stevenpollack <stevenpollack@users.noreply.github.com>
2020-04-03 11:35:15 +1100

    started working on double-click functionality

--- hw4/FontChooser.js
+++ hw4/FontChooser.js
@@ -3,6 +3,7 @@ class FontChooser extends React.Component {
 	super(props)
 
 	// have to bind like below to access `this` correctly.
+	this.handleTextDoubleClick = this.handleTextDoubleClick(this.handleTextDoubleClick)
 	this.handleTextClick = this.handleTextClick.bind(this.handleTextClick)
 	this.handleDecreaseButton = this.handleDecreaseButton.bind(this.handleDecreaseButton)
 	this.handleIncreaseButton = this.handleIncreaseButton.bind(this.handleIncreaseButton)
@@ -34,33 +35,57 @@ class FontChooser extends React.Component {
 
   // use the fxn form of setState() to make sure that previous state is used...
   handleTextClick = (event) => {
-    console.log(event.target)
-    this.setState((state) => ({
-      hideForm: !state.hideForm
-    }))
+	  // var timeoutID = setTimeout(fxn, delay)
+	  // clearTimeout(timeoutID)
+	  const dblClickDelay = 300 //ms
+	  var timeoutID = setTimeout( () => {
+		      this.setState((state) => ({
+				  hideForm: !state.hideForm
+				}))
+			}, dblClickDelay)
+	  this.setState( (state) => ({timeoutID: timeoutID}))
   }
 
   handleIncreaseButton = (event) => {
-	// var textSize = this.state.fontSize
-	console.log(this.maxSize)
 	this.setState( state => {
-		if (state.fontSize < state.maxSize) {
-			return {fontSize: state.fontSize+1}
-		} else if (state.fontSize == state.maxSize) {
+		if (state.fontSize == state.maxSize) {
+			return {} // do nothing and break
+		}
+
+		var newSize = state.fontSize+1
+		if (newSize == state.maxSize) {
 			return {
+				fontSize: newSize,
 				fontColor: 'green',
 				fontStyle: 'italic'
 				}
-		}
+		} else {
+			return {
+				fontStyle: 'normal',
+				fontSize: newSize,
+				fontColor: 'black'
+			}
+		} 
 	})
 	}
 
-	
-
-
   handleDecreaseButton = (event) => {
 	  var textSize = this.state.fontSize
-	  this.setState( (state) => ({ fontSize: state.fontSize-- }) )
+	  this.setState( (state) => {
+		  if (state.fontSize > state.minSize) {
+			  return { 
+				  fontSize: state.fontSize-1,
+				  fontStyle: 'normal',
+				  fontColor: 'black'
+				}
+		  } else if (state.fontSize == state.minSize) {
+			  return {
+				  fontWeight: 'bold',
+				  fontStyle: 'italic'
+			  }
+
+		  }
+		})
 	  console.log(textSize)
   }
 
@@ -71,17 +96,46 @@ class FontChooser extends React.Component {
 	  this.setState( (state) => ({ boldFont: state.boldFont === 'normal'? 'bold' : 'normal'}) )
 
   }
-  
+
+  handleTextDoubleClick = (event) => {
+	  // clear the single click event timeout call
+	  if (this.state.timeoutID) {
+		  clearTimeout(this.state.timeoutID)
+	  }
+
+	  this.setState((state) => {
+		  return {
+			  fontSize: this.props.size,
+			  fontWeight: this.props.bold ? 'bold' : 'normal',
+			  color: 'black'
+		  }
+	  })
+  }
+
 // implement for min
   render () {
     return (
       <div>
-        <input type='checkbox' id='boldCheckbox' onChange={this.handleCheckBox} hidden={this.state.hideForm} />
-        <button id='decreaseButton' onClick={this.handleDecreaseButton} hidden={this.state.hideForm}>-</button>
+        <span id='textSpan'
+		 onDoubleClick={this.handleTextDoubleClick}
+		 style={{
+			 fontStyle: this.state.fontStyle,
+			 color: this.state.fontColor,
+			 fontSize: this.state.fontSize,
+			 fontWeight: this.state.boldFont}}
+		  onClick={this.handleTextClick}>
+		  	{this.props.text}
+		</span>
+      	<br/>
+		  <label for="boldCheckbox" hidden={this.state.hideForm}>
+		  	<input type='checkbox' id='boldCheckbox' onChange={this.handleCheckBox} hidden={this.state.hideForm} />
+		  	Bold
+		  </label>
+
+		<button id='decreaseButton' onClick={this.handleDecreaseButton} hidden={this.state.hideForm}>-</button>
         <span id='fontSizeSpan' hidden={this.state.hideForm}>{this.state.fontSize}</span>
         <button id='increaseButton' onClick={this.handleIncreaseButton} hidden={this.state.hideForm}>+</button>
-        <span id='textSpan' style={{fontStyle: this.state.fontStyle, color: this.state.fontColor, fontSize: this.state.fontSize, fontWeight: this.state.boldFont}} onClick={this.handleTextClick}>{this.props.text}</span>
-      </div>
+        </div>
     )
   }
 }
